035ab4802258202e7949e616c9677b4099878f65
Max Carlquist <tenkaklet@gmail.com>
2024-01-29 10:53:49 +0100

    Refactor user input handling and API request creation

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,23 @@ use serde::Serialize;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
+    
+    // Prompt for user input and set the default value to an empty string
     let input_prompt = Input::new("Enter your prompt", |s| Ok(s.to_string()))
         .default_value("")
         .help_message("");
-
-    let name = input_prompt.display();
+    // Get the user input and store it in a variable
+    let user_input = input_prompt.display();
 
     dotenv::dotenv().ok();
     let _client = Client::new(dotenv::var("API_KEY").unwrap().to_string());
 
+    //  Create a request object with the user input to send to the API
     let txt_request = Request {
         contents: vec![Content {
             role: Role::User,
             parts: vec![Part {
-                text: Some(name.unwrap().to_string()),
+                text: Some(user_input.unwrap().to_string()),
                 inline_data: None,
                 file_data: None,
                 video_metadata: None,
@@ -43,6 +45,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         text: String,
     }
 
+
+    // Post the request to the API & get a response in a variable called gemresults
+    // Result of type PostResult is a struct that contains a text property of type String called text.
     let gemresults = _client.post(30, &txt_request).await?;
 
     // Pattern match to extract and display the text property
